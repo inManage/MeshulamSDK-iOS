d8b26b59cc409303b5b02f02ec70aab97cc433fa
amit azulay <amita@inmanage.net>
2021-05-26 01:11:10 +0300

    first commit

---
+++ Logger.swift
@@ -0,0 +1,21 @@
+//
+//  Logger.swift
+//  MeshulamSDK
+//
+//  Created by Inmanage LTD on 26/05/2021.
+//
+
+import Foundation
+
+public class Logger {
+    
+    public init(){}
+    
+    public func printLog() {
+        print("Hello")
+    }
+    
+    private func privateMathod() {
+        print("private")
+    }
+}
