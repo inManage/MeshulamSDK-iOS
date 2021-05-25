359cbd18a261eb73146eecb1deb56889da59d707
amit azulay <amita@inmanage.net>
2021-05-26 00:51:41 +0300

    Initial commit

---
+++ Example/MeshulamSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MeshulamSDK
+//
+//  Created by amita@inmanage.net on 05/26/2021.
+//  Copyright (c) 2021 amita@inmanage.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
