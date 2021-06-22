d12d2605c467d603ad0a2445a9f04ebbc64d430f
amit azulay <amita@inmanage.net>
2021-06-22 11:23:27 +0300

    add NSObject extension

--- Classes/Managers/PaymentManager.swift
+++ Classes/Managers/PaymentManager.swift
@@ -32,13 +32,4 @@ extension PaymentManager: RequestFinishedProtocol {
     }
 }
 
-extension NSObject {
-    
-    var className: String {
-        return String(describing: type(of: self))
-    }
-    
-    class var className: String {
-        return String(describing: self)
-    }
-}
+
