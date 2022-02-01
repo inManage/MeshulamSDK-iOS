df81c92b8d65b871b240ef7b10f7c7c9e1a2afc5
amit azulay <amita@inmanage.net>
2022-02-01 13:16:44 +0200

    change to live environment

--- MeshulamSDK/MeshulamSDK/Managers/MeshulamNetworkManager.swift
+++ MeshulamSDK/MeshulamSDK/Managers/MeshulamNetworkManager.swift
@@ -16,9 +16,9 @@ protocol NetworkManagerDelegate: NSObject {
 public class MeshulamNetworkManager: NSObject {
 
     public static let shared = MeshulamNetworkManager()
-
-    //public private(set) var baseURL: String = "https://plusecure.meshulam.co.il/"
-    public private(set) var baseURL: String = "https://dev.meshulam.co.il/"
+    //public private(set) var baseURL: String = "https://plusecure.meshulam.co.il/" plus
+    //public private(set) var baseURL: String = "https://dev.meshulam.co.il/" dev
+    public private(set) var baseURL: String = "https://meshulam.co.il/" //live
     public private(set) var applicationToken: String = ""
     public private(set) var maxTime : Int = 0
     public private(set) var startDelay : Int = 0
